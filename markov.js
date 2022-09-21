72a9ce20a23a5e3a6654b99a717fb219d2afe31c
JohnC <real.john.chueng@gmail.com>
2022-09-21 13:02:11 +0100

    secound commit

--- markov.js
+++ markov.js
@@ -6,9 +6,199 @@ import Mt from "./mt";
 const randomnizer = new Mt();
 
 
-class Markov { 
+class Markov {
     constructor(opts = {}) {
         this.n = opts.n || 3; //default n is 3
+        this.root = new Node(null, "ROOT");
+        this.maxAttempts = opts.maxAttempts || 999;
+        this.sentenceStarts = [];
+        this.sentenceEnds = new Set();
+        if (opts.text) this.addText(opts.text);
+    }
+
+    addText(text, multiplier = 1) {
+        // ideal data structure of text: [[token,token,...],[token,token,...]]
+        // if input is a string, define token separater:'/', sentence separater: ';'
+        if (typeof text === 'string') text = this.processRawText(text);
+
+        let allTokens = [];
+        for (let k = 0; k < multiplier; k++) {
+            for (let i = 0; i < text.length; i++) {
+                let tokens = text[i];
+                this.sentenceStarts.push(tokens[0]);
+                this.sentenceEnds.add(tokens[tokens.length - 1]);
+                allTokens.push(...tokens);
+            }
+            this.treeify(allTokens);
+        }
+    }
+
+    treeify(tokens) {
+        let root = this.root;
+        for (let i = 0; i < tokens.length; i++) {
+            let node = root;
+            let fragment = tokens.slice(i, i + this.n);
+            let wrap = 0;
+            for (let j = 0; j < this.n; j++) {
+                let hidden = false;
+                if (j >= fragment.length) {
+                    fragment[j] = tokens[wrap++];
+                    hidden = true
+                }
+                node = node.addChild(fragment[j]);
+                if (hidden) node.hidden = true;
+            }
+        }
+    }
+
+    * generate(count, opts = {}) {
+        // this generator yield a token each time, if count < 0, do it endlessly
+        // to smooth the outcome time, a buffer should be used
+
+        const num = count < 0 ? true : count;
+
+        let tries = 0, tokens = [], usedStarts = [];
+        let minIdx = 0, sentenceIdxs = [];
+        let markedNodes = [];
+
+        const unmarkNodes = () => {
+            markedNodes.forEach(n => n.marked = false);
+        }
+
+        const markNode = (node) => {
+            if (node) {
+                node.marked = tokens.reduce((acc, e) => acc + e.token, '');
+                markedNodes.push(node);
+            }
+        }
+
+        const resultCount = () => {
+            return tokens.filter(t => this._isEnd(t)).length;
+        }
+
+        const notMarked = (cn) => {
+            let tmap = tokens.reduce((acc, e) => acc + e.token, '');
+            return cn.marked !== tmap;
+        }
+
+        const fail = (msg, sentence, forceBacktrack) => {
+            tries++;
+            let sentIdx = sentenceIdx(); //?
+            sentence = sentence || this._flatten(tokens.slice(sentIdx)); //?
+            if (tries >= this.maxAttempts) throwError(tries, resultCount()); //?
+
+            let parent = this._pathTo(tokens);
+            let numOfChildren = parent ? parent.childNodes({ filter: notMarked }).length : 0;
+
+            if (forceBacktrack || numOfChildren === 0) {
+                backtrack();
+            }
+        }
+
+        const backtrack = () => {
+            let parent, tc;
+            for (let i = 0; i < 99; i++) {
+                let last = tokens.pop();
+                markNode(last);
+
+                if (this._isEnd(last)) sentenceIdxs.pop(); //?
+                let sentIdx = sentenceIdx();
+                let backtrackUntil = Math.max(sentIdx, minIdx);
+
+                parent = this._pathTo(tokens);
+                tc = parent.childNodes({ filter: notMarked });
+
+                if (tokens.length <= backtrackUntil) {
+
+                    if (minIdx > 0) { // have seed
+                        if (tokens.length <= minIdx) { // back at seed
+                            if (!tc.length) throw Error('back at barren-seed1: case 0');
+                            return true;
+                        }
+                        else { // back at sentence-start with seed
+                            if (!tc.length) {
+                                sentenceIdxs.pop();
+                            }
+                            else {  // continue
+                            }
+                        }
+                    }
+                    else {             // TODO: recheck
+                        if (!tokens.length) {
+                            sentenceIdxs = [];
+                            return selectStart();
+                        }
+                    }
+
+                    return true;
+                }
+
+                if (tc.length) {
+                    sentIdx = sentenceIdx();
+                    return parent;
+                }
+            }
+
+            throw Error('Invalid state in backtrack() [' + tokens.map(t => t.token) + ']');
+        }
+
+        const sentenceIdx = () => {
+            let len = sentenceIdxs.length;
+            return len ? sentenceIdxs[len - 1] : 0;
+        }
+
+        const selectStart = () => {
+            let seed = opts.seed;
+            // seed: ["token","token", ...], raw text: devided by '/'
+            if (seed && seed.length) {
+                if (typeof seed === 'string') seed = seed.split("/");
+                let node = this._pathTo(seed, this.root);
+                while (!node.isRoot()) {
+                    tokens.unshift(node);
+                    node = node.parent;
+                }
+            } else if (!tokens.length || this._isEnd(tokens[tokens.length - 1])) {
+                let usableStarts = this.sentenceStarts.filter(ss => notMarked(this.root.child(ss)));
+                if (!usableStarts.length) throw Error('No valid sentence-starts remaining');
+                let start = randomnizer.random(usableStarts);
+                let startTok = this.root.child(start);
+                markNode(startTok);
+                usableStarts = this.sentenceStarts.filter(ss => notMarked(this.root.child(ss)));
+                tokens.push(startTok);
+            } else {
+                throw Error('Invalid call to selectStart: ' + this._flatten(tokens));
+            }
+        }
+        //----------------------------------------------------------------------
+
+        selectStart();
+
+        // start generating
+        let endless = num === true
+        while (endless || resultCount() < num) {
+           // TODO
+        }
+    }
+
+    processRawText(raw) {
+        let expanded = raw.split(";");
+        for (let i = 0; i < expanded.length; i++) {
+            expanded[i] = expanded[i].split("/");
+        }
+        return expanded;
+    }
+
+
+    _pathTo() {
+
+    }
+
+    _flatten() {
+
+    }
+
+    _isEnd() {
+
     }
 }
 

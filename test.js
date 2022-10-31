5aa7d018cb752b88523cba846aa85a3e27ec7e23
JohnC <real.john.chueng@gmail.com>
2022-10-31 10:41:58 +0000

    add test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import Markov from "./markov.js";
 
-let testRaw = "這/是/一個/測試；兩個/句子;是/什麼/意思；這/人/有/什麼/問題；這/程式/不能/運行"
+let testRaw = "這/是/一個/測試；兩個/句子;是/什麼/意思；這/人/有/什麼/問題；這/程式/不能/運行;這/世界/在/崩潰；我們/的/世界/有/問題/出現"
 
 let model = Markov.makeModel({ n: 2, text: testRaw });
 
